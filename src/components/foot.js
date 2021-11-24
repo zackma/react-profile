3728734d0c889bde1eed571aac86e61930368166
zackma <804532840@qq.com>
2021-11-24 17:31:34 +0800

    ui amendments --fix

--- src/components/foot.js
+++ src/components/foot.js
@@ -8,7 +8,7 @@ export default function Footer() {
           id="footer"
           style={{ display:'flex', justifyContent:'center', width:'98%', marginTop:20 }}
         >
-          <Box style={{ position:'absolute', height:40, marginTop:80 }}>
+          <Box style={{ position:'absolute', height:40, marginTop:80, bottom:0 }}>
             <Typography style={{ color: '#9e9e9e',fontFamily: 'Arial',fontSize: '0.8rem' }}>
               Copyright @Zack Marcus | 
               <Link href="https://www.github.com/zackma">
